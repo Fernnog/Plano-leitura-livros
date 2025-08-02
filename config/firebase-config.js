5e045ba552771a02c944b5809327d10514e89113
Fernnog <fernandocnogueira@gmail.com>
2025-08-02 09:35:47 -0400

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -7,15 +7,22 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
 
-// Suas credenciais do Firebase.
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCzLjQrE3KhneuwZZXIost5oghVjOTmZQE",
-    authDomain: "plano-leitura.firebaseapp.com",
-    projectId: "plano-leitura",
-    storageBucket: "plano-leitura.appspot.com",
-    messagingSenderId: "589137978493",
-    appId: "1:589137978493:web:f7305bca602383fe14bd14"
+  apiKey: "AIzaSyAMaAx8Pal440RksEnVGiVI57w1H_dcEV0",
+  authDomain: "plano-de-leitura-de-livros.firebaseapp.com",
+  databaseURL: "https://plano-de-leitura-de-livros-default-rtdb.firebaseio.com",
+  projectId: "plano-de-leitura-de-livros",
+  storageBucket: "plano-de-leitura-de-livros.firebasestorage.app",
+  messagingSenderId: "329742565308",
+  appId: "1:329742565308:web:93f80cf919b7aa1fddb5b9",
+  measurementId: "G-XEK0TWRC98"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Inicializa o Firebase App
 const app = initializeApp(firebaseConfig);
